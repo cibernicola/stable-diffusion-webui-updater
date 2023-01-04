ed1fc8ed73d1b293b973a3ebbf3456cdd0d162fe
Marc <4579387+cibernicola@users.noreply.github.com>
2023-01-04 11:19:25 +0100

    Changes to be committed: 	renamed:    custom-updater.py -> scripts/custom-updater.py

--- scripts/custom-updater.py
+++ scripts/custom-updater.py
@@ -35,9 +35,9 @@ def ask():
 def update_extensions():
     import os
     counter=1
-    extensions_path= os.getcwd().replace("\stable-diffusion-webui-updater","")
+    extensions_path= os.getcwd().replace("\stable-diffusion-webui-updater\scripts","")
     extensions_path= os.path.join(extensions_path,"extensions")
-    
+    #extensions_path="L:\stable-diffusion-webui\extensions"
     extensions_list= os.listdir(extensions_path)
     extension_list=[]
     

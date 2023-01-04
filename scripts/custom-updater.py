78339c579796d4850e08f58d9e10821014b260df
Marc <4579387+cibernicola@users.noreply.github.com>
2023-01-04 17:23:08 +0100

    Changes to be committed: 	modified:   requirements.txt 	modified:   scripts/custom-updater.py

--- scripts/custom-updater.py
+++ scripts/custom-updater.py
@@ -1,37 +1,39 @@
 import git
+from colorama import Fore, Back, Style
+
 
 
 #heavyly based on original code from webUI
-def is_outdated(extension):
+def is_outdated(extension,extension_name):
     try:
         repo = git.Repo(extension)
         for fetch in repo.remote().fetch("--dry-run"):
             if fetch.flags != fetch.HEAD_UPTODATE:
-                print("==>",extension.split("\\")[-1],"outdated")
+                print(Fore.YELLOW + f"===> {extension_name},outdated!🤨")
                 return True
             return False
     except Exception as e:
-        print(f"===>Error checking {extension.split('\\')[-1]}. Please check it manually.<===")
+        print(Fore.RED + f'===>Error checking {extension_name}. Please check it manually🙄.<===')
         return False
 
 
 #heavyly based on original code from webUI
-def fetch_and_reset_hard(update_extension):
+def fetch_and_reset_hard(update_extension,extension_name):
     try:
         repo = git.Repo(update_extension)
         # Fix: `error: Your local changes to the following files would be overwritten by merge`,
         # because WSL2 Docker set 755 file permissions instead of 644, this results to the error.
         repo.git.fetch('--all')
         repo.git.reset('--hard', 'origin')
-        print("===>",update_extension.split("\\")[-1], "updated!")
+        print(Fore.GREEN + f"===> {extension_name},updated!😎")
     except Exception as e:
-        print(f'===>Error updating {update_extension.split("\\")[-1]}. Please check it manually.<===')
+        print(Fore.RED + f'===>Error updating {extension_name}. Please check it manually🙄.<===')
         return False
 
 def ask():
-    from inputimeout import inputimeout, TimeoutOccurred   
+    from inputimeout import inputimeout, TimeoutOccurred  
     try:
-        answer = inputimeout(prompt=str("\n Update WebUI extensions (y + Enter or Enter (Yes) / Any key (No)): "), timeout=10)
+        answer = inputimeout(Fore.WHITE + str("\n Update WebUI extensions (y + Enter or Enter (Yes) / Any key (No)): "), timeout=10)
         answer=str(answer).lower()
         if answer=="y" or answer=="":
             return True
@@ -46,20 +48,22 @@ def update_extensions():
     counter=1
     extensions_path= os.getcwd().replace("\stable-diffusion-webui-updater\scripts","")
     extensions_path= os.path.join(extensions_path,"extensions")
-
-    extensions_list= os.listdir(extensions_path)
-    extension_list=[]
+   
     
-    for extension in extensions_list :
+    raw_extensions_list= os.listdir(extensions_path)
+    cleaned_extension_list=[]
+    
+    for extension in raw_extensions_list :
         update_extension = os.path.join(extensions_path, extension)
         if os.path.isdir(update_extension):
-            extension_list.append(update_extension)
+            cleaned_extension_list.append(update_extension)
             
-    for extension in extension_list :
+    for extension in cleaned_extension_list :
+        extension_name=update_extension.split("\\")[-1]
         update_extension = os.path.join(extensions_path, extension)
-        print(f"Checking {counter} of {len(extensions_list)-1}:", update_extension.split("\\")[-1], )
-        if is_outdated(update_extension):
-            fetch_and_reset_hard(update_extension)
+        print(f"Checking {counter} of {len(cleaned_extension_list)-1}:", extension_name, )
+        if is_outdated(update_extension,extension_name):
+            fetch_and_reset_hard(update_extension,extension_name)
         counter+=1
 
 
